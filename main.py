b9ef3f1f7975510a9798c556e52606e46cd80ce8
Lowryder921 <116798401+Lowryder921@users.noreply.github.com>
2025-05-29 07:54:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -93,3 +93,17 @@ def fake_community_messages():
 threading.Thread(target=fake_community_messages, daemon=True).start()
 
 bot.infinity_polling()
+
+
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot Firos działa!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
